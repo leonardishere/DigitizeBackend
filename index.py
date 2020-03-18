207bc5fd9060ae798e9675df414451821314c5cb
leonardishere <leonardishere@gmail.com>
2020-03-18 13:33:58 -0700

    add multiple handlers for different requests

--- index.py
+++ index.py
@@ -1,12 +1,35 @@
 import json
 import datetime
 
-
-def handler(event, context):
+def get_handler(event, context):
     data = {
-        'output': 'Hello World',
+        'output': 'Hello World from GET /',
         'timestamp': datetime.datetime.utcnow().isoformat()
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
             'headers': {'Content-Type': 'application/json'}}
+
+def post_handler(event, context):
+    data = {
+        'output': 'Hello World from POST /',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
+def default_handler(event, context):
+    data = {
+        'error': 'Could not handle request {} {}'.format(event['httpMethod'], event['resource'])
+    }
+    return {'statusCode': 400,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}
+
+def handler(event, context):
+    if event['httpMethod'] == "GET" and event['resource'] == "/":
+        return get_handler(event, context)
+    if event['httpMethod'] == "POST" and event['resource'] == "/":
+        return post_handler(event, context)
+    return default_handler(event, context)

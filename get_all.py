78975623976c40a0f6457f43ff32f6347b712a08
leonardishere <leonardishere@gmail.com>
2020-03-18 14:24:05 -0700

    add get_all handler

---
+++ get_all.py
@@ -0,0 +1,11 @@
+import json
+import datetime
+
+def handler(event, context):
+    data = {
+        'output': 'Hello World from GET /students/',
+        'timestamp': datetime.datetime.utcnow().isoformat()
+    }
+    return {'statusCode': 200,
+            'body': json.dumps(data),
+            'headers': {'Content-Type': 'application/json'}}

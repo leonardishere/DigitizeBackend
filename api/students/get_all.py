7785e597dbb92f677c5c994eeb37878b3e8c0f93
leonardishere <leonardishere@gmail.com>
2020-03-19 06:55:23 -0700

    move xray to template

--- api/students/get_all.py
+++ api/students/get_all.py
@@ -5,10 +5,10 @@ import sys
 sys.path.append(os.path.join(os.curdir, "entities"))
 from student import Student
 
-from aws_xray_sdk.core import xray_recorder
-from aws_xray_sdk.core import patch_all
-xray_recorder.configure(context_missing='LOG_ERROR')
-patch_all()
+#from aws_xray_sdk.core import xray_recorder
+#from aws_xray_sdk.core import patch_all
+#xray_recorder.configure(context_missing='LOG_ERROR')
+#patch_all()
 
 # initiating the dynamodb client takes >200 ms. moving out here to init once and persist
 dynamodb_client = boto3.client('dynamodb', region_name='us-west-2')

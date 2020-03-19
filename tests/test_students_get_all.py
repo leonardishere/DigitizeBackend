1149500b2c3f5942a31479d08da4880c54d86ff6
leonardishere <leonardishere@gmail.com>
2020-03-19 04:45:43 -0700

    restructure, add more tests

---
+++ tests/test_students_get_all.py
@@ -0,0 +1,20 @@
+import unittest
+import os
+import sys
+sys.path.append(os.path.join(os.curdir, "api/students"))
+import get_all
+
+
+class TestHandlerCase(unittest.TestCase):
+
+    def test_response(self):
+        print("testing response.")
+        result = get_all.handler(None, None)
+        print(result)
+        self.assertEqual(result['statusCode'], 200)
+        self.assertEqual(result['headers']['Content-Type'], 'application/json')
+        self.assertIn('GET /students/', result['body'])
+
+
+if __name__ == '__main__':
+    unittest.main()

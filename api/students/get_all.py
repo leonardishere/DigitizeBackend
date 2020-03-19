4e4ee5dfc55d84551f1e63a81853132bedbfb2bf
leonardishere <leonardishere@gmail.com>
2020-03-19 06:18:35 -0700

    add xray

--- api/students/get_all.py
+++ api/students/get_all.py
@@ -5,6 +5,14 @@ import sys
 sys.path.append(os.path.join(os.curdir, "entities"))
 from student import Student
 
+from aws_xray_sdk.core import xray_recorder
+from aws_xray_sdk.core import patch_all
+xray_recorder.configure(
+    aws_xray_tracing_name='DigitizeBackend',
+    context_missing='LOG_ERROR'
+)
+patch_all()
+
 # initiating the dynamodb client takes >200 ms. moving out here to init once and persist
 dynamodb_client = boto3.client('dynamodb', region_name='us-west-2')
 

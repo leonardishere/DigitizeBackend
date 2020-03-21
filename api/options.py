6fca998d3dd2bad9b7ed5bd16fc73851f427b786
leonardishere <leonardishere@gmail.com>
2020-03-21 13:45:37 -0700

    add localhost to cors

--- api/options.py
+++ api/options.py
@@ -5,7 +5,7 @@ def handler(event, context):
     return {
         'statusCode': 200,
         'headers': {
-            "Access-Control-Allow-Origin": "https://digitize.aleonard.dev",
+            "Access-Control-Allow-Origin": "https://digitize.aleonard.dev,localhost",
             "Access-Control-Allow-Headers": "Content-Type",
             "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
         }

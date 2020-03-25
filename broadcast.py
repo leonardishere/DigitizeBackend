9120f6ce1714eb22d47351f473e983e131121d3c
leonardishere <leonardishere@gmail.com>
2020-03-25 04:28:48 -0700

    create broadcast function

---
+++ broadcast.py
@@ -0,0 +1,42 @@
+import json
+import boto3
+#import awscurl
+import subprocess
+
+#subprocess.run(["pip3", "install", "awscurl", "--user"])
+
+dynamodb_client = boto3.client('dynamodb', region_name='us-west-2')
+
+headers = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*", #"https://digitize.aleonard.dev",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+}
+
+def get_connections():
+    connections = dynamodb_client.scan(
+        TableName='DigitizeConnections'
+    )['Items']
+    print('connections:', connections)
+    return connections
+
+def broadcast(message, connections):
+    for connection in connections:
+        subprocess.run([
+            "awscurl",
+            "--region", "us-west-2",
+            "-X", "POST",
+            "-d", message,
+            "https://soagcpz8cl.execute-api.us-west-2.amazonaws.com/Prod/%40connections/"+connection['connectionId']['S']
+        ])
+
+# Lambda handler
+def handler(event, context):
+    message = event['body']
+    connections = get_connections()
+    broadcast(message, connections)
+    return {
+        'statusCode': 200,
+        'headers': headers
+    }
